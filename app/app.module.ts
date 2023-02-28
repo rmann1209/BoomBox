88ce3fda2de516cec1dae438ed900f73f95393aa
Robert Mann <91228383+rmann1209@users.noreply.github.com>
2023-02-28 15:48:19 -0500

    Add files via upload
    Updated home page UI to a more sleek design, and created buttons of type submit for backend rerouting integration.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
