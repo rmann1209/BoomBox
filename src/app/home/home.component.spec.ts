c22f1f8341037c567477a07db02787bf283c3c05
AdamKing402 <118691521+AdamKing402@users.noreply.github.com>
2023-03-29 18:41:36 -0400

    Add files via upload

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -17,7 +17,4 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,3 +1,4 @@
+/*
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
@@ -9,6 +10,37 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+});
+*/
+
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { loginUser } from '../login/login.service';
+
+import { LoginComponent } from './login.component';
+
+describe('SignInComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginComponent ],
+      imports: [HttpClientModule, MatCardModule, ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
+      providers: [loginUser]
     })
     .compileComponents();
 
