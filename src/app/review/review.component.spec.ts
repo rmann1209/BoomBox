e9c54449ec4c993970655ea38c6637631d7988b5
Robert Mann <91228383+rmann1209@users.noreply.github.com>
2023-04-18 18:53:21 -0400

    Add files via upload
    Added review page functionality

--- src/app/review/review.component.spec.ts
+++ src/app/review/review.component.spec.ts
@@ -1,6 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// import { ReviewComponent } from './review.component';
+
+// describe('ReviewComponent', () => {
+//   let component: ReviewComponent;
+//   let fixture: ComponentFixture<ReviewComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ ReviewComponent ]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(ReviewComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { ReviewComponent } from './review.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterOutlet } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ReviewComponent', () => {
   let component: ReviewComponent;
@@ -8,6 +38,8 @@ describe('ReviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule,
+        HttpClientTestingModule, RouterOutlet, FormsModule, ReactiveFormsModule ],
       declarations: [ ReviewComponent ]
     })
     .compileComponents();
@@ -17,7 +49,13 @@ describe('ReviewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('check if even', async(() => {
+    expect(2==2).toBeTruthy();
+  }));
+
+  it('should create the review component', async(() => {
+    const fixture = TestBed.createComponent(ReviewComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 });
