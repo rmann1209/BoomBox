1a3c2e2ad963317c4021276711489ae28e75f205
Robert Mann <91228383+rmann1209@users.noreply.github.com>
2023-04-18 17:32:45 -0400

    Add files via upload
    Added functionality for review page

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent {
+  searchQuery: string;
+  showReview: boolean;
+  review: string;
+  selectedStar: number;
+  hoveredStar: number;
+  stars: number[];
 
+  constructor() {
+    this.searchQuery = '';
+    this.showReview = false;
+    this.review = '';
+    this.selectedStar = 0;
+    this.hoveredStar = 0;
+    this.stars = [1, 2, 3, 4, 5];
+  }
+
+  search(): void {
+    // your search logic here
+    this.showReview = true;
+  }
+
+  selectStar(star: number): void {
+    this.selectedStar = star;
+  }
+
+  submitReview(): void {
+    // your review submission logic here
+    this.review = '';
+    this.selectedStar = 0;
+    this.hoveredStar = 0;
+    this.showReview = false;
+  }
 }
