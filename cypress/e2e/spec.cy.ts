d9c48994bbebf2fb6f9395d6d7923c460249cd8d
AdamKing402 <118691521+AdamKing402@users.noreply.github.com>
2023-04-19 16:30:07 -0400

    Cypress Updates

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,10 +1,11 @@
 describe('template spec', () => {
   it('Local Host Opened', () => {
     cy.visit('http://localhost:4200/');
-    cy.url().should('eq', 'http://localhost:4200/')
+    cy.url().should('eq', 'http://localhost:4200/home')
 
     //Cypress.on('uncaught:exception', (err, runnable) => { return false; });
   })
+
   it('Test Home Routing', () => {
     cy.visit('http://localhost:4200/');
     cy.get('#home-button').click();
@@ -33,21 +34,38 @@ describe('template spec', () => {
     cy.visit('http://localhost:4200/');
     cy.get('#login-button').click();
     cy.get('#Username').type('test');
-    cy.get('#Password').type('testpass');
+    cy.get('#Password').type('test');
     cy.get('#submit').click();
+    cy.url().should('eq', 'http://localhost:4200/review')
   })
 
   it('Test Sign Up then Login', () => {
     cy.visit('http://localhost:4200/');
     cy.get('#signup-button').click();
     cy.get('#Username').type('test');
-    cy.get('#Password').type('testpass');
+    cy.get('#Password').type('test');
     cy.get('#submit').click();
 
     cy.get('#login-button').click();
     cy.get('#Username').type('test');
-    cy.get('#Password').type('testpass');
+    cy.get('#Password').type('test');
     cy.get('#submit').click();
+    cy.url().should('eq', 'http://localhost:4200/review');
+  })
+
+
+  it('Test Login then Review', () => {
+    cy.visit('http://localhost:4200/');
+    cy.get('#login-button').click();
+    cy.get('#Username').type('test');
+    cy.get('#Password').type('test');
+    cy.get('#submit').click();
+
+    cy.get('#search-input').type('Taylor Swift');
+    cy.get('#Confirm-button').click();
+    cy.get('#review-input').type('Incredible Music!');
+    cy.get('.star-rating > :nth-child(5)').click();
+    cy.get('#submit-button').click();
   })
 
   it('Review Functionality', () => {
@@ -60,7 +78,7 @@ describe('template spec', () => {
     cy.get('#search-input').type('Taylor Swift');
     cy.get('#Confirm-button').click();
     cy.get('#review-input').type('Cool Beans!');
-    cy.get('.star-rating > :nth-child(5)').click;
+    cy.get('.star-rating > :nth-child(5)').click();
     cy.get('#submit-button').click();
   })
 
